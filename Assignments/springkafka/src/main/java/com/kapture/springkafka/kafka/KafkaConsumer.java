749282a35b79003534f6cb8db26bf6fd852b428a
yadunandanbhat <yadunandanbhat@gmail.com>
2022-06-09 13:19:58 +0530

    Working Kafka demo

---
+++ Assignments/springkafka/src/main/java/com/kapture/springkafka/kafka/KafkaConsumer.java
@@ -0,0 +1,16 @@
+package com.kapture.springkafka.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "demoTopic", groupId = "demoGroup")
+    public void consumer(String message) {
+        LOGGER.info(String.format("Message received -> %s", message));
+    }
+}
